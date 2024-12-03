a2285e8959e0eb68beecc58c491f6e2ba5b86a85
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-12-03 19:36:44 +0200

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+
+  typescript: {
+    overrides: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+
+  rules: {
+    'antfu/if-newline': 'off',
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+})
